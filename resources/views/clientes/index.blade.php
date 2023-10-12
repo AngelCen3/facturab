62d52538dfa15173c53fa6df2b01936c889d6f4d
BetoRobert <wyjawy@hotmail.com>
2023-10-12 12:36:34 -0500

    CRUD done

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,43 +1,127 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight flex items-center justify-between">
-            {{ __('Clientes') }}
-            <a href="{{ route('clientes.create') }}" class="text-xs bg-gray-800 text-white rounded px-4 py-2">
-                Nuevo Cliente
-            </a>
+       
+
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <!--Tabla Clientes-->
+        <div class="w-full mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __('Listado de Clientes') }}
-
-                    <table class="mb-4">
-                        @foreach ($clientes as $cliente)
-                            <tr class="border-b border-gray-200 text-sm">
-                                <td class="px-6 py-4"> {{ $cliente->nombre }} </td>
-                                <td class="px-6 py-4">
-                                    <a href="{{ route('clientes.edit', $cliente) }}" class="text-indigo-600">Editar</a>
-
-                                </td>
-                                <td class="px-6 py-4">
-                                    <form action=" {{ route('clientes.destroy', $cliente) }}" method="POST">
-                                        <!--Hace que nuestro formulario sea seguro genera un token de seguridad-->
-                                        @csrf
-                                        <!--Le decimos que nuestra intencion es eliminar-->
-                                        @method('DELETE')
-
-                                        <input type="submit" value="Eliminar"
-                                            class="bg-gray-800 text-white rounded px-4 py-2"
-                                            onclick="return confirm('Desea eliminar?')">
-                                    </form>
-                                </td>
+                    Index.blade
+                    <!--Tabla de contenido-->
+                    <div class="flex justify-between items-center py-4">
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            {{ __('Clientes') }} 
+                        </h2>
+                        <a href="{{ route('clientes.create') }}"
+                            class="text-xs bg-gray-800 text-white rounded px-4 py-2">
+                            Nuevo Cliente
+                        </a>
+                    </div>
+                    <table class="min-w-full">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Id
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Nombre
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Contacto
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Form. Pago
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Met. Pago
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Tel. Cel
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Tel. Loc
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Correo 1
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Correo 2
+                                </th>
+                                <th
+                                    class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
+                                    Direccion
+                                </th>
+                                <!-- Agrega más encabezados de columna según tus necesidades -->
                             </tr>
-                        @endforeach
+                        </thead>
+                        <tbody>
+                            @foreach ($clientes as $cliente)
+                                <tr>
+                                    <td class="px-6 py-4">
+                                        <a href="{{ route('clientes.edit', $cliente) }}" class="text-indigo-600">Editar</a>
+    
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <form action=" {{ route('clientes.destroy', $cliente) }}" method="POST">
+                                            <!--Hace que nuestro formulario sea seguro genera un token de seguridad-->
+                                            @csrf
+                                            <!--Le decimos que nuestra intencion es eliminar-->
+                                            @method('DELETE')
+    
+                                            <input type="submit" value="Eliminar"
+                                                class="bg-gray-800 text-white rounded px-4 py-2"
+                                                onclick="return confirm('Desea eliminar?')">
+                                        </form>
+                                    </td>
+                                    <td class="px-6 py-4 bg-gray-600 text-white whitespace-nowrap">                                          
+                                        {{ $cliente->id }}
+                                    </td>
+                                    <td class="px-6 py-4 bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->nombre }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->contacto }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->forma_pago }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->metodo_pago }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->tel_celular }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->tel_local }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->correouno }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->correodos }}
+                                    </td>
+                                    <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
+                                        {{ $cliente->direccion }}
+                                    </td>
+
+                                    <!-- Agrega más columnas según tus necesidades -->
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
-                    {{ $clientes->links() }}
+
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,12 @@
-@extends('template')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('FacturaHome.blade') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-<div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8  gap-10 py-12">
-    <div class="mt-4 flex justify-center">
-        <!--Botones-->
-        <button class="px-4 py-2 bg-slate-500 text-white font-bold rounded-md ml-2">Crear</button>
-    </div>
     <div class="flex">
         <div class="w-64">
-            <!--Dashboard-->
             <a href=""
                 class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">
                 Emisores
@@ -57,7 +55,7 @@
                     </a>
                 </li>
             </ul>
-    
+
             <a href=""
                 class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">
                 Configuracion
@@ -71,87 +69,5 @@
                 Salir del Sistema
             </a>
         </div>
-        <div class="w-full overflow-x-auto mx-5">
-            <!--Tabla de contenido-->
-            <h1 class="text-2xl font-bold text-white mb-4">Tabla de Clientes</h1>
-            <table class="min-w-full">
-                <thead>
-                    <tr>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Id
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Nombre
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Contacto
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Form. Pago
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Met. Pago
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Tel. Cel
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Tel. Loc
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Correo 1
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Correo 2
-                        </th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium uppercase tracking-wider">
-                            Direccion
-                        </th>
-                        <!-- Agrega más encabezados de columna según tus necesidades -->
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($clientes as $cliente)
-                        <tr>
-                            <td class="px-6 py-4 bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->id }}
-                            </td>
-                            <td class="px-6 py-4 bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->nombre }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->contacto }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->forma_pago }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->metodo_pago }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->tel_celular }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->tel_local }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->correouno }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->correodos }}
-                            </td>
-                            <td class="px-6 py-4m bg-gray-600 text-white whitespace-nowrap">
-                                {{ $cliente->direccion }}
-                                    <a href="{{ route('clientes.edit', $cliente) }}" class="text-indigo-600">Editar</a>                                                         
-                            </td>
-    
-                            <!-- Agrega más columnas según tus necesidades -->
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            
-        </div>
     </div>
-      
-</div>
+</x-app-layout>
